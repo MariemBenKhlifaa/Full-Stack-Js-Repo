465e67000e09fd9cc86a072716c1638843ddb685
DorsafCharfeddine <dorsaf.charfeddine@esprit.tn>
2023-05-10 18:58:03 +0100

    corsss

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(
   })
 );
 
-app.use(cors({ credentials: true, origin: "*" }));
+app.use(cors({ credentials: true, origin: "https://youthconnect2.onrender.com" }));
 
 app.use(cookieParser());
 app.use(express.json());
